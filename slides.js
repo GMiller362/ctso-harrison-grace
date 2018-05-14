e4f7f863b5bd09cfcaba7f149bf2d17d1c341787
Harrison Stephens <hstephens333@west-mec.org>
2018-05-14 15:22:57 -0700

    Slideshow
    Attempted to get the slideshow working, hoever it doesnt stay active when the page is refreshed.

--- slides.js
+++ slides.js
@@ -1,9 +1,30 @@
-// var slides = document.querySelectorAll('#slides .slide');
-// var currentSlide = 0;
-// var slideInterval = setInterval(nextSlide, 2000);
-//
-// function nextSlide() {
-//   slides[currentSlide].className = 'slide';
-//   currentSlide = (currentSlide + 1) % slides.length;
-//   slides[currentSlide].className = 'slide showing';
-// }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}

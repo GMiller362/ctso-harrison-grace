6c0dd7f0035d1573577ea4864a4c7cd2238fc115
Unknown <GMiller362@west-mec.org>
2018-05-15 15:15:22 -0700

    Auto stash before merge of "master" and "ctso-atlas/master"

--- slides.js
+++ slides.js
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 
 var slideIndex = 0;
 showSlides();
+=======
+var slideIndex = 1;
+showSlides(slideIndex);
+>>>>>>> Stashed changes
 
 // Next/previous controls
 function plusSlides(n) {
@@ -13,6 +18,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+<<<<<<< Updated upstream
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
@@ -22,6 +28,21 @@ function showSlides(n) {
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
+=======
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+>>>>>>> Stashed changes
 }
 
 // var slideIndex = 1;

baf95999f0b8d6428b18a017774a66eed01e269a
Unknown <hstephens333@west-mec.org>
2018-08-21 14:05:10 -0700

    Heres Ya Code

--- validation.js
+++ validation.js
@@ -1,43 +1,69 @@
 
-window.addEventListener("load", function() {
-  document.getElementById("submit").onclick = runSubmit;
-  document.getElementById("fName").oninput = validateFirstName;
-  document.getElementById("lName").oninput = validateLastName;
-  document.getElementById("email").oninput = validateEmail;
-})
+"use strict"
 
-function runSubmit() {
-  validateFirstName();
-  validateLastName();
-  validateEmail();
+// Global Variables
+var formValidity = true;
+
+// Function to validate required fields
+function validateRequired() {
+      var inputElements = document.querySelectorAll("#contactinfo input");
+      var errorDiv = document.getElementById("errorText");
+      var fieldsetValidity = true;
+      var elementCount = inputElements.length;
+      var currentElement;
+      try {
+            // Loop through looking for blanks
+            for (var i = 0; i < elementCount; i++) {
+                  currentElement = inputElements[i];
+                  // Blanks
+                  if (currentElement.value === "") {
+                        currentElement.style.background = "rgb(255, 233, 233)";
+                        fieldsetValidity = false;
+                  } else {
+                        currentElement.style.background = "white";
+                        errorDiv.style.display = "none";
+                  }
+            }
+            if (fieldsetValidity === false) {
+                  throw "Please Fill Out Everything";
+            }
+      } catch (msg) {
+            errorDiv.style.display = "block";
+            errorDiv.innerHTML = msg;
+            formValidity = false;
+      }
 }
 
-function validateFirstName() {
-  var firstName = document.getElementById("fName");
-  if (firstName.validity.valueMissing) {
-    firstName.setCustomValidity("Enter your first name.");
-  }
-  else {
-    firstName.setCustomValidity("");
-  }
+// Function to validate the entire form
+function validateForm(evt) {
+      if (evt.preventDefault) {
+            evt.preventDefault();
+      } else {
+            evt.returnValue = false;
+      }
+      formValidity = true;
+
+      // Function Calls
+      validateRequired();
+
+      if (formValidity === true) {
+            document.getElementsByTagName("form")[0].submit();
+      }
 }
 
-function validateLastName() {
-  var lastName = document.getElementById("lName");
-  if (lastName.validity.valueMissing) {
-    lastName.setCustomValidity("Enter your last name.");
-  }
-  else {
-    lastName.setCustomValidity("");
-  }
+// Function to create Event Listeners
+function createEventListeners() {
+      var form = document.getElementsByTagName("form")[0];
+      if (form.addEventListener) {
+            form.addEventListener("submit", validateForm, false);
+      } else if (form.attachEvent) {
+            form.attachEvent("onsubmit", validateForm);
+      }
 }
 
-function validateEmail() {
-  var emailVar = document.getElementById("email");
-  if (emailVar.validity.valueMissing) {
-    emailVar.setCustomValidity("Enter your e-mail.");
-  }
-  else {
-    emailVar.setCustomValidity("");
-  }
+// Page loads event handlers   
+if (window.addEventListener) {
+      window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+      window.attachEvent("onload", createEventListeners);
 }

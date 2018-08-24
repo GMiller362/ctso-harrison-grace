fb705c15d71cd5c5dd5c6671b3028f650ebf18c7
Unknown <hstephens333@west-mec.org>
2018-08-24 14:01:15 -0700

    Form Validation Code

--- validation.js
+++ validation.js
@@ -4,6 +4,82 @@
 // Global Variables
 var formValidity = true;
 
+// Function to validate answer box
+function validateAnswer() {
+      var answerBox = document.getElementById("answer");
+      var errorDiv = document.getElementById("errorText");
+      var fieldsetValidity = true;
+      try {
+          // Validate textarea
+          if (answerBox.value === "" || answerBox.value === answerBox.placeholder) {
+            answerBox.style.background = "rgb(255, 233, 233)";
+            fieldsetValidity = false;
+          }
+          else {
+              answerBox.style.background = "white";
+          }
+      }
+      catch (msg) {
+          errorDiv.style.display = "block";
+          errorDiv.innerHTML = msg;
+          formValidity = false;
+      }
+  }
+  
+// Function to Validate Select Lists
+function validateSelectList() {
+      var selectElements = document.querySelectorAll("#contactinfo" + " select");
+      var errorDiv = document.getElementById("errorText");
+      var fieldsetValidity = true;
+      var elementCount = selectElements.length;
+      var currentElement;
+      try {
+          // Loop through input fields looking for blanks
+          for (var i = 0; i < elementCount; i++) {
+              currentElement = selectElements[i];
+              // Blanks
+              if (currentElement.selectedIndex === 0) {
+                  currentElement.style.background = "rgb(255, 233, 233)";
+                  fieldsetValidity = false;
+              }
+              // Not Blanks
+              else {
+                  currentElement.style.background = "white";
+              }
+          }
+      }
+      catch (msg) {
+          errorDiv.style.display = "block";
+          errorDiv.innerHTML = msg;
+          formValidity = false;
+      }
+  }  
+
+// Function to Validate Radio Buttons
+function validateRadioButtons() {
+      var radioElements = document.getElementsByName("contact");
+      var errorDiv = document.getElementById("errorText");
+      var fieldsetValidity = true;
+      try {
+            // Validate radio buttons one must be on
+            if (!radioElements[0].checked && !radioElements[1].checked) {
+                  for (var i = 0; i < radioElements.length; i++) {
+                        radioElements[i].style.outline = "1px solid red";
+                  }
+                  fieldsetValidity = false;
+            }
+            else {
+                  for (var i = 0; i < radioElements.length; i++) {
+                        radioElements[i].style.outline = "";
+                  }
+            }
+      } catch (msg) {
+            errorDiv.style.display = "block";
+            errorDiv.innerHTML = msg;
+            formValidity = false;
+      }
+}
+
 // Function to validate required fields
 function validateRequired() {
       var inputElements = document.querySelectorAll("#contactinfo input");
@@ -45,6 +121,9 @@ function validateForm(evt) {
 
       // Function Calls
       validateRequired();
+      validateRadioButtons();
+      validateSelectList();
+      validateAnswer();
 
       if (formValidity === true) {
             document.getElementsByTagName("form")[0].submit();
